173acea52aa0d50bb7c59ab3cd5e5f2d8c67d62a
faith okpala <okpalafaith1@gmail.com>
2022-08-09 14:30:53 +0100

    added starter and worked on auth

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+import createError, { HttpError } from  'http-errors';
+import express, { NextFunction } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from  'morgan';
+// import dotenv from 'dotenv';
+
+import indexRouter from './routes/index';
+import transactionRouter from './routes/transaction';
+import authRouter from './routes/authRoute';
+import usersRouter from './routes/users';
+
+const app = express();
+
+dotenv.config({ path: 'config.env'})
+
+// view engine setup
+app.set('views', path.join(__dirname,'..', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'..', 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/v1/transaction', transactionRouter);
+app.use("/api/v1/auth", authRouter);
+app.use('/api/v1/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err: any, req:any, res:any, next:any) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
